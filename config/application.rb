b698700d36202d5a4d49abda7fa44755c3179614
sanyakosivchuk <kosivcuks@gmail.com>
2025-05-16 23:04:36 +0300

    rubocop

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Collegeteamproject
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-    config.i18n.available_locales = [:en, :uk]
+    config.i18n.available_locales = [ :en, :uk ]
     config.i18n.default_locale    = :en
 
     # Configuration for the application, engines, and railties goes here.
